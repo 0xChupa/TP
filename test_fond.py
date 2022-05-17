c04a8820e6f285e05b52ce9ae06f850b1fa00434
test_fond_1 <maxencecontant6@gmail.com>
2022-05-17 17:10:46 +0200

    test fond graphique v2

--- test_fond.py
+++ test_fond.py
@@ -2,16 +2,22 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
-
+x = 640
+y = 480
 #Ouverture de la fenêtre Pygame
-fenetre = pygame.display.set_mode((640, 480))
-
+fenetre = pygame.display.set_mode((x, y))
+fenetre = pygame.display.set_mode((x,y), RESIZABLE)
+#fenetre = pygame.display.set_mode((640,480), FULLSCREEN)
 #Chargement et collage du fond
-fond = pygame.image.load("back.jpg").convert()
-fenetre.blit(fond, (0,0))
+#fond = pygame.image.load("back.jpg").convert()
+#fenetre.blit(fond, (0,0))
 
-perso = pygame.image.load("bingo.jpg").convert()
-fenetre.blit(perso, (20,30))
+bingo = pygame.image.load("bingooo.jpg").convert()
+#bingo.set_colorkey((255,255,255))
+fenetre.blit(bingo, (15,15))
+
+Roulette = pygame.image.load("Roulette.jpg").convert()
+fenetre.blit(Roulette, (441,15))
 
 #Rafraîchissement de l'écran
 pygame.display.flip()
@@ -25,7 +31,10 @@ while continuer:
         if event.type == KEYDOWN and event.key == K_SPACE:
             continuer = 0
 
-        if event.type == MOUSEBUTTONDOWN and event.button == 3 and event.pos[1] < 100:
-            print("Zone dangereuse")
+        if event.type == MOUSEBUTTONDOWN and event.button == 3 and event.pos[0] > 15 and event.pos[0] < 199 and event.pos[1] > 15 and event.pos[1] < 103:
+            print("début du bingo...")
+
+        if event.type == MOUSEBUTTONDOWN and event.button == 3 and event.pos[0] > 441 and event.pos[0] < 625 and event.pos[1] > 15 and event.pos[1] < 103:
+            print("début de la Roulette...")
 
 
