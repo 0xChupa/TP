f5fb1af43361d81b74f7239882c2a633c5665675
Chupa <alex01.tavernier@gmail.com>
2022-05-17 16:30:30 +0200

    Correction de Bugs

--- bingo.py
+++ bingo.py
@@ -1,16 +1,15 @@
 import array
 import random
 
-balance = input("Avec combien commencez-vous à jouer ? ")
-
-choicesP = [i for i in range(1,90)]
-choicesB = [i for i in range(1,90)]
-randomNumList = [i for i in range(1,90)]
-
 def check(list): 
     return all(i == list[0] for i in list) 
 
-playBoardP = [         #P pour Player
+def playBingo(balance):
+    choicesP = [i for i in range(1,90)]
+    choicesB = [i for i in range(1,90)]
+    randomNumList = [i for i in range(1,90)]
+
+    playBoardP = [         #P pour Player
             [0,0,0,0,0],
             [0,0,0,0,0],
             [0,0,0,0,0],
@@ -18,7 +17,7 @@ playBoardP = [         #P pour Player
             [0,0,0,0,0],
         ]
 
-playBoardB = [         # B pour Bot
+    playBoardB = [         # B pour Bot
             [0,0,0,0,0],
             [0,0,0,0,0],
             [0,0,0,0,0],
@@ -26,7 +25,7 @@ playBoardB = [         # B pour Bot
             [0,0,0,0,0],
         ]
 
-for i in range(5):
+    for i in range(5):
         for j in range(5):
             choiceP = random.choice(choicesP)       #Remplissage du Tableau Player
             playBoardP[i][j] = choiceP
@@ -37,40 +36,40 @@ for i in range(5):
             choicesB.pop(choicesB.index(choiceB))
 
 
-mise = input("Quelle est votre mise ? ")
-while int(mise) > int(balance):
-    mise = input("Votre mise est supérieure à votre balance. Rentrez une nouvelle mise : ")
-balance = int(balance) - int(mise)
-
-for i in range (5):
-    print(str(playBoardP[i]) + "             "   + str(playBoardB[i]))
-
-while check(playBoardB) == False and check(playBoardP) == False:
-    randomNum = random.choice(randomNumList)
-    randomNumList.pop(randomNumList.index(randomNum))
-    print(randomNum)
-
-    for i in range(5):
-        for j in range(5):
-            if playBoardP[i][j] == randomNum:
-                playBoardP[i][j] = 'X'
-            if playBoardB[i][j] == randomNum:
-                playBoardB[i][j] = 'X'
-            else:
-                continue
+    mise = input("Quelle est votre mise ? ")
+    while int(mise) > int(balance):
+        mise = input("Votre mise est supérieure à votre balance. Rentrez une nouvelle mise : ")
+    balance = int(balance) - int(mise)
 
     for i in range (5):
         print(str(playBoardP[i]) + "             "   + str(playBoardB[i]))
-    if check(playBoardB) == True and check(playBoardP) == True:
-        print("Vous avez rempli votre grille en même temps, égalite !")
-        balance = int(balance) + int(mise)
-        break
-    if check(playBoardP) == True:
-        print("Vous avez gagné, bien joué !")
-        mise = int(mise) * 2
-        balance = int(balance) + int(mise)
-        break
-    if check(playBoardB) == True:
-        print("Votre adversaire a gagné, dommage !")
 
-print("Votre balance est désormais : " + str(balance))
+    while check(playBoardB) == False and check(playBoardP) == False:
+        randomNum = random.choice(randomNumList)
+        randomNumList.pop(randomNumList.index(randomNum))
+        print(randomNum)
+
+        for i in range(5):
+            for j in range(5):
+                if playBoardP[i][j] == randomNum:
+                    playBoardP[i][j] = 'X'
+                if playBoardB[i][j] == randomNum:
+                    playBoardB[i][j] = 'X'
+                else:
+                    continue
+
+        for i in range (5):
+            print(str(playBoardP[i]) + "             "   + str(playBoardB[i]))
+        if check(playBoardB) == True and check(playBoardP) == True:
+            print("Vous avez rempli votre grille en même temps, égalite !")
+            balance = int(balance) + int(mise)
+            break
+        if check(playBoardP) == True:
+            print("Vous avez gagné, bien joué !")
+            mise = int(mise) * 2
+            balance = int(balance) + int(mise)
+            break
+        if check(playBoardB) == True:
+            print("Votre adversaire a gagné, dommage !")
+
+    return balance
