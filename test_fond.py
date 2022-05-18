82dfdf17b0f24954c10d68b2ef4043ada6ef522b
test_fond_1 <maxencecontant6@gmail.com>
2022-05-18 16:10:34 +0200

    écran d'accueuil v2 ( ajout de la musique )

--- test_fond.py
+++ test_fond.py
@@ -25,28 +25,35 @@ fenetre.blit(Blackjack, (15,330))
 Machine = pygame.image.load("Rr.jpg").convert()
 fenetre.blit(Machine, (441,280))
 
+file = 'musique.mp3'
+musique = pygame.mixer.music.load("musique.mp3")
+pygame.mixer.music.play()
+
 #Rafraîchissement de l'écran
 pygame.display.flip()
 
 #BOUCLE INFINIE
 continuer = 1
 while continuer:
-    for event in pygame.event.get():   #On parcours la liste de tous les événements reçus
+    for event in pygame.event.get():   #On parcours la liste de tous les événements
         if event.type == QUIT:
             continuer = 0
         if event.type == KEYDOWN and event.key == K_ESCAPE:
             continuer = 0
 
         if event.type == MOUSEBUTTONDOWN and event.button == 3 and event.pos[0] > 15 and event.pos[0] < 180 and event.pos[1] > 15 and event.pos[1] < 120:
+            pygame.mixer.music.stop()
             print("début du bingo...")
 
         if event.type == MOUSEBUTTONDOWN and event.button == 3 and event.pos[0] > 441 and event.pos[0] < 625 and event.pos[1] > 15 and event.pos[1] < 103:
             print("début de la Roulette...")
+            pygame.mixer.music.stop()
 
         if event.type == MOUSEBUTTONDOWN and event.button == 3 and event.pos[0] > 0 and event.pos[0] < 199 and event.pos[1] > 250 and event.pos[1] < 450:
             print("début du blackjack...")
+            pygame.mixer.music.stop()
 
         if event.type == MOUSEBUTTONDOWN and event.button == 3 and event.pos[0] > 441 and event.pos[0] < 625 and event.pos[1] > 250 and event.pos[1] < 450:
             print("début de la machine à sous...")
-
+            pygame.mixer.music.stop()
 
